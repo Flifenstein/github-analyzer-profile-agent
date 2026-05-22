97a9116c6ecfb36531731ca6de7f63a83a784e1c
flifenstein <ioanafrincu@outlook.com>
2026-05-22 18:05:19 +0200

    realized i want to score the repos individually so i had to refactor that. and fix the .gitignore lol

--- schemas/score_repo.py
+++ schemas/score_repo.py
@@ -1,12 +1,15 @@
 # score should be bteween 1 and 10, with 10 being top notch developers repo level that is creative, clean, easy to fork. 
 # 2 is a incomplete repo with, bad readme.md, not reproducible, bad messy code
-#my aim is to score somewhere on btw 7-8
+#my aim is to score somewhere on btw 7-8 per repo
+#realized i should add a profile score separatley and rename this ScoreProfile to socreRepo
 
 from pydantic import BaseModel, Field
 
-class ScoreProfile(BaseModel):
+class RepoScore(BaseModel):
     name: str
     score: int = Field (ge=1, le=10)
-    hasreadme: bool
+    has_readme: bool
+    
+
 
 
--- tests/test_score_repo.py
+++ tests/test_score_repo.py
@@ -1,31 +1,31 @@
-
-
-from schemas.score_profile import ScoreProfile
+from schemas.score_repo import RepoScore
 from pydantic import ValidationError
 import pytest
 
 
-class TestScoreProfile:
+class TestRepoScore:
     def test_valid_score(self):
-        score = ScoreProfile(name="Name-repo", score=8, hasreadme=True)
+        score = RepoScore(name="Name-repo", score=8, has_readme=True)
         assert score.name == "Name-repo"
         assert score.score == 8
         assert score.has_readme is True
 
     def test_invalid_name(self):
         with pytest.raises(ValidationError):
-            ScoreProfile(name=9, score=11, has_readme=True)
+            RepoScore(name=9, score=8, has_readme=True)
 
     def test_invalid_score_outofbound(self):
         with pytest.raises(ValidationError):
-            ScoreProfile(name="Name-repo", score=11, has_readme=True)
+            RepoScore(name="Name-repo", score=11, has_readme=True)
+
     def test_invalid_score_invalid(self):
         with pytest.raises(ValidationError):
-            ScoreProfile(name="Name-repo", score="high", has_readme=True)
+            RepoScore(name="Name-repo", score="high", has_readme=True)
+
     def test_invalid_score_negative(self):
         with pytest.raises(ValidationError):
-            ScoreProfile(name="Name-repo", score=-1, has_ readme=True)
-    
-    def test_invalid_hasreadme(self):
+            RepoScore(name="Name-repo", score=-1, has_readme=True)
+
+    def test_invalid_has_readme(self):
         with pytest.raises(ValidationError):
-            ScoreProfile(name="Name-repo", score=8, hasreadme="maybe")
+            RepoScore(name="Name-repo", score=8, has_readme="maybe")
